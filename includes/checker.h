f511a61228d09d37844a63ddaf027e1052f6e1fa
gcros <guilhem.cros.pro@gmail.com>
2024-01-22 02:02:01 +0100

    oops

--- includes/checker.h
+++ includes/checker.h
@@ -1,23 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_load.h                                          :+:      :+:    :+:   */
+/*   checker.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/07 11:10:18 by gcros             #+#    #+#             */
-/*   Updated: 2024/01/21 20:52:16 by gcros            ###   ########.fr       */
+/*   Created: 2024/01/22 00:01:14 by gcros             #+#    #+#             */
+/*   Updated: 2024/01/22 01:13:08 by gcros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_LOAD_H
-# define FT_LOAD_H
+#ifndef FT_CHECKER_H
+# define FT_CHECKER_H
 
-# include "arr.h"
+# include "ft_operation.h"
 
-t_array	*ft_load(int ac, char **av);
-int		normalizer(t_array **array);
-int		*dup_pint(void *value);
-int		ft_check_dup(t_array *array);
+t_array	*ft_get_operation();
+int	ft_apply_op(t_sort *sort);
+
 
 #endif
